70f2d44714d98d0d778622c9d04c6fe517f5bbe3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-04 17:14:25 +0900

    add benchmark test

--- gort_bench_test.go
+++ gort_bench_test.go
@@ -1,1 +1,53 @@
 package gort
+
+import (
+	"sort"
+	"testing"
+)
+
+func BenchmarkGortWithArray(b *testing.B) {
+	b.ResetTimer()
+
+	data := make([]int, 10)
+
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < len(data); i++ {
+			data[i] = i ^ 0xcccc
+		}
+
+		Sort(&data, len(data), func(i, j int) bool {
+			return data[i] > data[j]
+		})
+	}
+}
+
+func BenchmarkForWithSlice(b *testing.B) {
+	b.ResetTimer()
+
+	data := make([]int, 10)
+
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < len(data); i++ {
+			data[i] = i ^ 0xcccc
+		}
+		Sort(&data, len(data), func(i, j int) bool {
+			return data[i] > data[j]
+		})
+	}
+}
+
+func BenchmarkDefaultSortWithSlice(b *testing.B) {
+	b.ResetTimer()
+
+	data := make([]int, 10)
+
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < len(data); i++ {
+			data[i] = i ^ 0xcccc
+		}
+
+		sort.Slice(data, func(i, j int) bool {
+			return data[i] > data[j]
+		})
+	}
+}
