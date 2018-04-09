3072542cd64aa7bd91b2e7505020a85a7cc56ab9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-10 00:58:52 +0900

    refactor

--- gort.go
+++ gort.go
@@ -9,7 +9,8 @@ import (
 func Sort(object interface{}, len int, less func(i, j int) bool) {
 	switch reflect.TypeOf(object).Elem().Kind() {
 	case reflect.Array:
-		rvArray := reflect.Indirect(reflect.ValueOf(&object)).Elem().Elem()
+		v := reflect.ValueOf(&object)
+		rvArray := reflect.Indirect(v).Elem().Elem()
 
 		if len < 2 || len > rvArray.Len() {
 			return
@@ -18,7 +19,8 @@ func Sort(object interface{}, len int, less func(i, j int) bool) {
 		sort.Slice(rvArray.Slice(0, len).Interface(), less)
 
 	case reflect.Slice:
-		rvSlice := reflect.Indirect(reflect.ValueOf(&object)).Elem().Elem()
+		v := reflect.ValueOf(&object)
+		rvSlice := reflect.Indirect(v).Elem().Elem()
 		sort.Slice(rvSlice.Slice(0, len).Interface(), less)
 	}
 }
